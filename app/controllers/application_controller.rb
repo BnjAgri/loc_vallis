29b22a6722b82a22e7e206678004096e0019fe80
benjamin masson <benjamin.masson@gmail.com>
2026-01-05 16:41:46 +0100

    Add Pundit, Stripe, and Devise Owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  # With two Devise scopes (Owner + User), Pundit needs a single actor.
+  def pundit_user
+    current_owner || current_user
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
+  end
 end
