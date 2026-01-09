6c53b54f45abc3e5dc8625b8411935952a7ee117
benjamin masson <benjamin.masson@gmail.com>
2026-01-09 17:45:22 +0100

    Add Mapbox map card to rooms index

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr" # @4.6.13
 pin "mustache" # @4.2.0
 pin "sweetalert2", to: "https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.esm.all.min.js", preload: true
+pin "mapbox-gl", to: "https://esm.sh/mapbox-gl@2.15.0"
