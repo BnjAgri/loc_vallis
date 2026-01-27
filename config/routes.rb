d93901d2a28baf96628678cad2a95a2451353135
benjamin masson <benjamin.masson@gmail.com>
2026-01-27 18:22:46 +0100

    Fix Devise password reset redirect + i18n
    - Add session route helpers mapped to unified login
    
    - Add missing Devise FR translations for passwords

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,21 @@ Rails.application.routes.draw do
     devise_for :owners, skip: [:sessions]
     devise_for :users, skip: [:sessions]
 
+    # Devise expects session route helpers (e.g. new_user_session_path) even if
+    # we use a unified login/logout UI. Provide the standard Devise session
+    # routes as aliases to UnifiedSessionsController.
+    devise_scope :user do
+      get "users/sign_in", to: "unified_sessions#new", as: :new_user_session
+      post "users/sign_in", to: "unified_sessions#create", as: :user_session
+      delete "users/sign_out", to: "unified_sessions#destroy", as: :destroy_user_session
+    end
+
+    devise_scope :owner do
+      get "owners/sign_in", to: "unified_sessions#new", as: :new_owner_session
+      post "owners/sign_in", to: "unified_sessions#create", as: :owner_session
+      delete "owners/sign_out", to: "unified_sessions#destroy", as: :destroy_owner_session
+    end
+
     get "login", to: "unified_sessions#new"
     post "login", to: "unified_sessions#create"
     get "login/email_exists", to: "unified_sessions#email_exists"
