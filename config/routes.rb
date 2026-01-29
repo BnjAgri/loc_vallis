2681004c03ddf2e1f575f085fa40c3e1629bc87d
benjamin masson <benjamin.masson@gmail.com>
2026-01-29 18:39:09 +0100

    Notifications: mark read on click

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
     get "login/email_exists", to: "unified_sessions#email_exists"
     delete "logout", to: "unified_sessions#destroy"
 
+    get "notifications/mark_read", to: "notifications#mark_read", as: :mark_notifications_read
+
     root to: "rooms#index"
 
     get "legal", to: "pages#legal"
